763d155417fd62510338746b90c878912a40320a
Tan <25267490+TanZng@users.noreply.github.com>
2021-05-31 18:39:15 -0500

    Add incomes and categories incomes to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts 'Run seeds'
 user = Users::User.create(id: 1, email: 'mail@mail.com', password: '123456', password_confirmation: '123456')
-wallet = Users::Wallet.create(id: 1, name: 'my wallet')
-user.wallet_id = wallet.id
+
+Users::Wallet.create(id: 1, name: 'my wallet', user_id: 1)
+
+user.wallet_id = 1
+user.save
+
+Categories::CategoryIncome.create(id: 1, title: 'Trabajo', color: '#FF9E33', icon: 'laptop', wallet_id: 1)
+Categories::CategoryIncome.create(id: 2, title: 'Beca', color: '#33BCFF', icon: 'school', wallet_id: 1)
+Categories::CategoryIncome.create(id: 3, title: 'Inversiones', color: '#FF5733', icon: 'rocket', wallet_id: 1)
+
+FinancialObjects::Income.create(id: 1, description: 'Gasto 1', currency: 12.22, category_id: 1, wallet_id: 1)
+FinancialObjects::Income.create(id: 2, description: 'Gasto 2', currency: 342.22, category_id: 2, wallet_id: 1)
+FinancialObjects::Income.create(id: 3, description: 'Gasto 3', currency: 1255.22, category_id: 3, wallet_id: 1)
