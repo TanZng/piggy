1b23a20041ea08b46fba174c3b2f9bd96a139c31
Tan <25267490+TanZng@users.noreply.github.com>
2021-06-02 13:34:38 -0500

    Refactor dashboard controller

--- app/controllers/dashboards/dashboard_controller.rb
+++ app/controllers/dashboards/dashboard_controller.rb
@@ -1,19 +1,31 @@
-class Dashboards::DashboardController < ApplicationController
-  def monthly
-    @month_and_year = if params[:month].nil? || params[:month] == ''
-                        Time.now.strftime('%Y-%m')
-                      else
-                        params[:month]
-                      end
-    @month = Date::MONTHNAMES[@month_and_year.split('-')[1].to_i]
-    @year = @month_and_year.split('-')[0]
-  end
+module Dashboards
+  class DashboardController < ApplicationController
+    before_action :set_month_and_year, only: :monthly
+    before_action :set_year, only: :annual
 
-  def annual
-    @year = if params[:year].nil? || params[:year] == ''
-              Time.now.strftime('%Y')
-            else
-              params[:year]
-             end
+    def monthly; end
+
+    def annual; end
+
+    private
+
+    def set_year
+      @year = if params[:year].nil? || params[:year] == ''
+                Time.now.strftime('%Y')
+              else
+                params[:year]
+              end
+    end
+
+    def set_month_and_year
+      @month_and_year = if params[:month].nil? || params[:month] == ''
+                          Time.now.strftime('%Y-%m')
+                        else
+                          params[:month]
+                        end
+      @month = Date::MONTHNAMES[@month_and_year.split('-')[1].to_i]
+      @year = @month_and_year.split('-')[0]
+    end
+
   end
 end
