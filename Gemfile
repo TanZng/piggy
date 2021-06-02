14f830b2d34b256bfe88588dec672ea4d8068c1b
Tan <25267490+TanZng@users.noreply.github.com>
2021-06-02 09:42:43 -0500

    Add faker gem
    + Add faker gem to improve our seeds.rb

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'factory_bot_rails'
   gem 'rspec-rails', '~> 5.0.0'
+  gem 'faker', '~> 2.18'
   # Help to kill N+1 queries and unused eager loading
   gem 'bullet'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,54 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
-puts 'Run seeds'
+puts 'Run seed'
+
+#
+# user 1
+#
 user1 = Users::User.create(email: 'mail@mail.com', password: '123456', password_confirmation: '123456')
 
 wallet1 = Users::Wallet.create(name: 'my wallet 1', user_id: user1.id)
 
 user1.wallet_id = wallet1.id
 user1.save
-
-c1 = Categories::CategoryIncome.create(title: 'Trabajo', color: '#FF9E33', icon: 'laptop', wallet_id: wallet1.id)
-c2 = Categories::CategoryIncome.create(title: 'Beca', color: '#33BCFF', icon: 'school', wallet_id: wallet1.id)
-c3 = Categories::CategoryIncome.create(title: 'Inversiones', color: '#FF5733', icon: 'rocket', wallet_id: wallet1.id)
-
-FinancialObjects::Income.create(description: 'Ingreso 1', currency: 12.22, category_id: c1.id, wallet_id: wallet1.id)
-FinancialObjects::Income.create(description: 'Ingreso 2', currency: 342.22, category_id: c2.id, wallet_id: wallet1.id)
-FinancialObjects::Income.create(description: 'Ingreso 3', currency: 1255.22, category_id: c3.id, wallet_id: wallet1.id)
-FinancialObjects::Income.create(description: 'Ingreso 4', currency: 12.22, category_id: c1.id, wallet_id: wallet1.id)
-FinancialObjects::Income.create(description: 'Ingreso 5', currency: 342.22, category_id: c2.id, wallet_id: wallet1.id)
-FinancialObjects::Income.create(description: 'Ingreso 6', currency: 1255.22, category_id: c3.id, wallet_id: wallet1.id)
-FinancialObjects::Income.create(description: 'Ingreso 7', currency: 12.22, category_id: c1.id, wallet_id: wallet1.id)
-FinancialObjects::Income.create(description: 'Ingreso 8', currency: 342.22, category_id: c2.id, wallet_id: wallet1.id)
-FinancialObjects::Income.create(description: 'Ingreso 9', currency: 1255.22, category_id: c3.id, wallet_id: wallet1.id)
-FinancialObjects::Income.create(description: 'Ingreso 10', currency: 12.22, category_id: c1.id, wallet_id: wallet1.id)
-FinancialObjects::Income.create(description: 'Ingreso 11', currency: 342.22, category_id: c2.id, wallet_id: wallet1.id)
-FinancialObjects::Income.create(description: 'Ingreso 12', currency: 1255.22, category_id: c3.id, wallet_id: wallet1.id)
-
-
-c4 = Categories::CategoryExpense.create(title: 'Mercado', color: '#FF9E33', icon: 'bag', wallet_id: wallet1.id)
-c5 = Categories::CategoryExpense.create(title: 'Luz', color: '#33BCFF', icon: 'bulb', wallet_id: wallet1.id)
-c6 = Categories::CategoryExpense.create(title: 'Ropa', color: '#FF5733', icon: 'shirt', wallet_id: wallet1.id)
-
-p1 = FinancialObjects::PaymentMethod.create(title: 'Tarjeta de credito', description: 'Mi tarjeta roja', wallet_id: wallet1.id)
-p2 = FinancialObjects::PaymentMethod.create(title: 'Tarjeta de debito', description: 'Mi tarjeta azul', wallet_id: wallet1.id)
-p3 = FinancialObjects::PaymentMethod.create(title: 'Efectivo', description: 'bajo el colchon', wallet_id: wallet1.id)
-
-
-FinancialObjects::Expense.create(description: 'Gasto 1', currency: 200.53, category_id: c4.id, wallet_id: wallet1.id, payment_method_id: p1.id)
-FinancialObjects::Expense.create(description: 'Gasto 2', currency: 42.26, category_id: c5.id, wallet_id: wallet1.id, payment_method_id: p2.id)
-FinancialObjects::Expense.create(description: 'Gasto 3', currency: 55.52, category_id: c6.id, wallet_id: wallet1.id, payment_method_id: p3.id)
-FinancialObjects::Expense.create(description: 'Gasto 4', currency: 200.53, category_id: c4.id, wallet_id: wallet1.id, payment_method_id: p1.id)
-FinancialObjects::Expense.create(description: 'Gasto 5', currency: 42.26, category_id: c5.id, wallet_id: wallet1.id, payment_method_id: p2.id)
-FinancialObjects::Expense.create(description: 'Gasto 6', currency: 55.52, category_id: c6.id, wallet_id: wallet1.id, payment_method_id: p3.id)
-FinancialObjects::Expense.create(description: 'Gasto 7', currency: 200.53, category_id: c4.id, wallet_id: wallet1.id, payment_method_id: p1.id)
-FinancialObjects::Expense.create(description: 'Gasto 8', currency: 42.26, category_id: c5.id, wallet_id: wallet1.id, payment_method_id: p2.id)
-FinancialObjects::Expense.create(description: 'Gasto 9', currency: 55.52, category_id: c6.id, wallet_id: wallet1.id, payment_method_id: p3.id)
-FinancialObjects::Expense.create(description: 'Gasto 10', currency: 200.53, category_id: c4.id, wallet_id: wallet1.id, payment_method_id: p1.id)
-FinancialObjects::Expense.create(description: 'Gasto 11', currency: 42.26, category_id: c5.id, wallet_id: wallet1.id, payment_method_id: p2.id)
-FinancialObjects::Expense.create(description: 'Gasto 12', currency: 55.52, category_id: c6.id, wallet_id: wallet1.id, payment_method_id: p3.id)
 
 #
 # user 2
@@ -64,23 +29,36 @@ wallet2 = Users::Wallet.create(name: 'my wallet 2', user_id: user2.id)
 user2.wallet_id = wallet2.id
 user2.save
 
-c1 = Categories::CategoryIncome.create(title: 'Trabajo', color: '#FF9E33', icon: 'laptop', wallet_id: wallet2.id)
-c2 = Categories::CategoryIncome.create(title: 'Beca', color: '#33BCFF', icon: 'school', wallet_id: wallet2.id)
-c3 = Categories::CategoryIncome.create(title: 'Inversiones', color: '#FF5733', icon: 'rocket', wallet_id: wallet2.id)
+30.times do
+  Categories::CategoryIncome.create(title: Faker::Company.name, color: Faker::Color.hex_color,
+                                    icon: Faker::Creature::Animal.name, wallet_id: wallet1.id)
 
-FinancialObjects::Income.create(description: 'Ingreso 1', currency: 12.22, category_id: c1.id, wallet_id: wallet2.id)
-FinancialObjects::Income.create(description: 'Ingreso 2', currency: 342.22, category_id: c2.id, wallet_id: wallet2.id)
-FinancialObjects::Income.create(description: 'Ingreso 3', currency: 1255.22, category_id: c3.id, wallet_id: wallet2.id)
+  Categories::CategoryExpense.create(title: Faker::Commerce.department(max: 1, fixed_amount: true), color: Faker::Color.hex_color,
+                                     icon: Faker::Creature::Animal.name, wallet_id: wallet1.id)
 
-c4 = Categories::CategoryExpense.create(title: 'Super', color: '#FF9E33', icon: 'bag', wallet_id: wallet2.id)
-c5 = Categories::CategoryExpense.create(title: 'Agua', color: '#33BCFF', icon: 'bulb', wallet_id: wallet2.id)
-c6 = Categories::CategoryExpense.create(title: 'Internet', color: '#FF5733', icon: 'shirt', wallet_id: wallet2.id)
+  FinancialObjects::PaymentMethod.create(title: "#{Faker::Business.credit_card_type} #{Faker::Color.color_name}",
+                                         description: Faker::Bank.name, wallet_id: wallet1.id)
 
-p1 = FinancialObjects::PaymentMethod.create(title: 'Tarjeta de credito 1', description: 'Mi tarjeta roja', wallet_id: wallet2.id)
-p2 = FinancialObjects::PaymentMethod.create(title: 'Tarjeta de debito 2', description: 'Mi tarjeta azul', wallet_id: wallet2.id)
-p3 = FinancialObjects::PaymentMethod.create(title: 'Efectivo', description: 'bajo el colchon', wallet_id: wallet2.id)
+  Categories::CategoryIncome.create(title: Faker::Company.name, color: Faker::Color.hex_color,
+                                    icon: Faker::Creature::Animal.name, wallet_id: wallet2.id)
 
+  Categories::CategoryExpense.create(title: Faker::Commerce.department(max: 1, fixed_amount: true), color: Faker::Color.hex_color,
+                                     icon: Faker::Creature::Animal.name, wallet_id: wallet2.id)
 
-FinancialObjects::Expense.create(description: 'Gasto 1', currency: 200.53, category_id: c4.id, wallet_id: wallet2.id, payment_method_id: p1.id)
-FinancialObjects::Expense.create(description: 'Gasto 2', currency: 42.26, category_id: c5.id, wallet_id: wallet2.id, payment_method_id: p2.id)
-FinancialObjects::Expense.create(description: 'Gasto 3', currency: 55.52, category_id: c6.id, wallet_id: wallet2.id, payment_method_id: p3.id)
+  FinancialObjects::PaymentMethod.create(title: "#{Faker::Business.credit_card_type} #{Faker::Color.color_name}",
+                                         description: Faker::Bank.name, wallet_id: wallet2.id)
+end
+
+60.times do
+  FinancialObjects::Income.create(description: Faker::Company.industry, currency: Faker::Number.decimal(l_digits: 2),
+                                  category_id: rand(1..30), wallet_id: wallet1.id)
+
+  FinancialObjects::Expense.create(description: Faker::Commerce.product_name, currency: Faker::Commerce.price, category_id: rand(1..30),
+                                   wallet_id: wallet1.id, payment_method_id: rand(1..30))
+
+  FinancialObjects::Income.create(description: Faker::Company.industry, currency: Faker::Number.decimal(l_digits: 2),
+                                  category_id: rand(1..30), wallet_id: wallet2.id)
+
+  FinancialObjects::Expense.create(description: Faker::Commerce.product_name, currency: Faker::Commerce.price, category_id: rand(1..30),
+                                   wallet_id: wallet2.id, payment_method_id: rand(1..30))
+end
