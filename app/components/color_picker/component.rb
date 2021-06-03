72887199b8ec23c16e3e66f04371269968d4f17a
Tan <25267490+TanZng@users.noreply.github.com>
2021-06-03 09:27:34 -0500

    Update label for ColorPicker

--- app/components/color_picker/component.rb
+++ app/components/color_picker/component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class ColorPicker::Component < ViewComponent::Base
-  def initialize(klass:, attribute_name:, color:)
+  def initialize(klass:, form:, attribute_name:, color:)
     super()
-    @klass, = klass,
+    @klass, = klass
+    @form = form
     @attribute_name = attribute_name
     @color = color
   end
